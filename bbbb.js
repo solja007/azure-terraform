ee28eeadb1ec882abeec303700ce00b33e526259
solja007 <zoran2709@gmail.com>
2018-09-25 15:46:50 +0200

    Create bbbb.js

---
+++ bbbb.js
@@ -0,0 +1,33 @@
+'use strict';
+
+module.exports = {
+
+ /* Find the average of three integers */
+ computeAverage: (num1, num2, num3) => {
+   let average;
+   // your logic goes here
+   return average;
+ },
+ 
+ /* Find the factorial of an integer */
+ computeFactorial: (num) => {
+   let factorial;
+   // your logic goes here
+   return factorial;
+ },
+
+ /* Convert a given Celcius temperature to Fahrenheit */
+ convertTempCtoF: (cTemp) => {
+   let fTemp;
+   // your logic goes here
+   return fTemp;
+ },
+
+ /* Convert a given Fahrenheit temperature to Celcius */
+ convertTempFtoC: (fTemp) => {
+   let cTemp;
+   // your logic goes here
+   return cTemp;
+ }
+
+}
